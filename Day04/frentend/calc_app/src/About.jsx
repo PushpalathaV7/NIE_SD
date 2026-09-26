b01b36a3d69b475a72ba8e13bd821cd7bf74ab20
PushpalathaV7 <pushpalathav176@gmail.com>
2026-09-26 13:24:10 +0530

    react

---
+++ Day04/frentend/calc_app/src/About.jsx
@@ -0,0 +1,8 @@
+export default function About()
+{
+     return (
+    <>
+     I am Pushpalatha V ,I am a student of NIE SD Batch 2026. I am currently learning React JS and I am excited to build web applications using this technology.
+     </>
+     )
+}
---
+++ Day04/frentend/calc_app/src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./About"
+ import Square from "./Square" 
+import SumCalc from "./SunCalc"
+ function App() 
+ { 
+  return ( 
+  
+  
+  <>
+  
+   <p>My Application</p>
+    <hr/> 
+    <Square/> 
+    <hr/>
+    <SumCalc/>
+     </> 
+     ) 
+    }
+      export default App
